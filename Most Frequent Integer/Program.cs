8c70e028c716443cd55dcfb2e8aeab2288c6ebe7
thomaswilliams4129 <thwilliams4129@gmail.com>
2020-02-27 11:26:07 -0500

    Completed Coding Challenge of Returning most Frequent Integer in a Array

---
+++ Most Frequent Integer/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Author: Thomas Williams
+Descritpion:
+    Given an array of integers, find the most frequent integer.
+    Write a method that takes an array of integers and returns an integer
+*/
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Job_Enterview_Questions
+{
+    class Program
+    {
+        /* Method returns the most Frequent Intger in a Array. */
+        public int mostFrequentInteger(int[] array) {
+            int mostFrequentInteger = 0;
+            int mostFrequentCount = 0;
+            /* Stores the Integer and its Frequency
+                * Key = Integer
+                * Value = Count
+            */
+            Dictionary<int, int> countOfNumbersInArray = new Dictionary<int, int>();
+
+            // Adds each integer from the array => countOfNumbersInArray
+            // Then increments its count 
+            // If the integer is already in the Dictionary increment its current count by 1
+            foreach(var integer in array) {
+                if(countOfNumbersInArray.ContainsKey(integer)) {
+                    countOfNumbersInArray[integer] += 1;
+                } else {
+                    countOfNumbersInArray.Add(integer, 1);
+                }
+            }
+
+            // Loop through countOfNumbersInArray to find the most frequent integer
+            foreach(var integer in countOfNumbersInArray) {
+                if(integer.Value > mostFrequentCount) {
+                    mostFrequentCount = integer.Value;
+                    mostFrequentInteger = integer.Key;
+                }
+            }
+            return mostFrequentInteger;
+        }
+
+        static void Main(string[] args) {
+            int[] array = new int[] {1, 9, 7, 2, 4, 5, 6, 2, 9, 2, 5, 3, 1, 6, 8, 5, 6, 1, 4, 6};
+            Program program = new Program();
+            
+            int returnResults= program.mostFrequentInteger(array);
+            Console.WriteLine(returnResults);
+        }
+    }
+}
+
+/*
+Given an array of integers, find the most frequent integer.
+Write a method that takes an array of integers and returns an integer
+*/
+
+
+
